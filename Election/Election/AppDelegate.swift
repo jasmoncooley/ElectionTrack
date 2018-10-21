aa3de3ab7a076dd10df49a11707532a2685e5889
Dalila Scott <Dalila@dalilas-macbook-pro.local>
2018-10-21 00:22:15 -0400

    pods

--- Election/Election/AppDelegate.swift
+++ Election/Election/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase
+//import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+        //FirebaseApp.configure()
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let randomViewController = UIViewController()
+        randomViewController.view.backgroundColor = .purple
+        let layout = UICollectionViewLayout()
+      //  let swipingController = SwipingController(collectionViewLayout:UICollectionViewLayout)
+        //window?.rootViewCOntroller = randomViewController
         return true
     }
 
---
+++ Election/Election/SwipingController.swift
@@ -0,0 +1,18 @@
+//
+//  SwipingController.swift
+//  Election
+//
+//  Created by Dalila Scott on 10/21/18.
+//  Copyright © 2018 Dalila Scott. All rights reserved.
+//
+
+import Foundation UIKit
+class name: UICollectionViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green 
+        
+    }
+    
+}
