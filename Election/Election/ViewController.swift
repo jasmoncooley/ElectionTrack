8485b0e244add85289837faeb183f6e1dc15e317
Dalila Scott <Dalila@dalilas-macbook-pro.local>
2018-10-20 20:28:02 -0400

    content

---
+++ Election/Election/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Election
+//
+//  Created by Dalila Scott on 10/20/18.
+//  Copyright © 2018 Dalila Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
